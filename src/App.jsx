1fdba7da52574bd3742692296adbd0f30867ddb2
pvamsi-del <poonamallivamsi2@gmail.com>
2025-10-21 10:26:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
+import "./Style.css";
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
+
+  return(
+    <div>
+      <h1>Task Focus</h1>
+      <p>Our friendly TaskManager</p>
+      <Taskform addTask = {addTask}/>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+      <button>Clear all tasks</button>
+    </div>
+  )
+}
